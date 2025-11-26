cb52d1e73e90601e851bbd6b4a3a8ffbe054a23b
Sarah Beatriz <sarah.nascimento02052007@gmail.com>
2025-11-26 13:46:16 +0000

    lista8A concluida

--- Lista8A_Streamlit/equacao.py
+++ Lista8A_Streamlit/equacao.py
@@ -5,21 +5,25 @@ class EquacaoGrauDois:
         self.a = a
         self.b = b
         self.c = c
+        if a == 0: 
+            raise ValueError("O valor deve ser maior que 0, senão não é uma equação de 2° grau")
 
     def calc_delta(self):
         return (self.b**2) - (4 * self.a * self.c)
+    
     def x1(self):
         delta = math.sqrt(self.calc_delta()) # raiz quadrada de delta
         return ((-self.b) + delta) // (2 * self.a) # -self.b nega o valor. se self.b for igual a 5, passará a ser -5
+    
     def x2(self):
-        delta = math.sqrt(self.calc_delta()) # raiz quadrada de delta
+        delta = math.sqrt(self.calc_delta())
         return ((-self.b) - delta) // (2 * self.a)
-    def y1(self):
-        return (self.a * self.x1()) + self.b + self.c
-    def y2(self):
-        return (self.a * self.x2()) + self.b + self.c
+    
+    def ponto_inflexao(self):
+        return -self.b / (2 * self.a)
+    
+    def y(self, x):
+        return (self.a * x * x) + (self.b*x) + self.c
 
     def __str__(self):
         return f"A = {self.a} \nB = {self.b} \nC = {self.c}"
-
-    
--- Lista8A_Streamlit/index.py
+++ Lista8A_Streamlit/index.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from equacao import EquacaoGrauDois
+import pandas as pd
 
 st.header("Calculadora de equação de 2º grau")
 a = st.text_input("Insira o valor de A")
@@ -10,3 +11,23 @@ if st.button("Calcular"):
     st.write(f"Delta = {eq.calc_delta()}")
     st.write(f"x1 = {eq.x1()}")
     st.write(f"x2 = {eq.x2()}")
+
+
+    p = eq.ponto_inflexao()
+    xmin = p - 10
+    xmax = p + 10
+    npontos = 50
+    dist = (xmax - xmin) / (npontos - 1)
+
+    px = []
+    py = []
+
+    x = xmin
+    while x <= xmax:
+        y = eq.y(x)
+        px.append(x)
+        py.append(y)
+        x += dist
+
+    df = pd.DataFrame({"x": px, "y": py})
+    st.line_chart(df, x="x", y="y")
